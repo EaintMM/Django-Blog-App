77264b5e40bb6c307b6f6f31d150e33c8e7f4f9a
EaintMM <eaintmyintmon@gmail.com>
2026-06-30 02:24:23 +0700

    Changed settins  for pythonanywhere 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-t41vvs!xizm=+auvnv3t)d$y_43vd#9930!@a+6=i)r8ge41-9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','.pythonanywhere.com','zaramills.pythonanywhere.com']
 
 
 # Application definition
